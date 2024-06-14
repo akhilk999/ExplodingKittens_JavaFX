5c9c1b412494b530e5d9146dcc086365c85d7d91
AndrewYLi11 <andrew.335633@gmail.com>
2024-06-13 17:03:52 -1000

    fixed maven build

---
+++ src/main/java/com/example/newexplodingkittens/applicationLauncher.java
@@ -0,0 +1,8 @@
+package com.example.newexplodingkittens;
+
+public class applicationLauncher {
+
+        public static void main(String[] args) {
+            GameApplication.main(args);
+        }
+}
